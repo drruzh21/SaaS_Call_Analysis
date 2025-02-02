c788e3201733d0798245fd86aa14401dfbdb96cb
Егор Дружинин <drruzh@MacBook-Pro.local>
2025-02-01 16:11:51 -0800

    finally works with views

--- backend/app/app/models/__init__.py
+++ backend/app/app/models/__init__.py
@@ -2,3 +2,4 @@ from .user import User  # noqa
 from .token import Token  # noqa
 from .api_key import APIKey  # noqa
 from .call_analysis import CallAnalysisResult, Objection  # noqa
+from . import events  # noqa: F401
