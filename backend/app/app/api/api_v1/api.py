d87ba9c7fdd810a431cac0fc74f7a3a0a39f5223
Andrey Zherdetskiy <aszherdetskiy@gmail.com>
2025-02-02 14:50:43 +0400

    Reg api_keys router

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -4,9 +4,11 @@ from app.api.api_v1.endpoints import (
     login,
     users,
     proxy,
+    api_keys,
 )
 
 api_router = APIRouter()
+api_router.include_router(api_keys.router, prefix="/api-keys", tags=["api-keys"])
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(proxy.router, prefix="/proxy", tags=["proxy"])
