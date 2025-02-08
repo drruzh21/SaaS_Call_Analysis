fba846f950bb80488249482bf3f0370696216062
drruzh <egordr0433@gmail.com>
2025-02-07 16:15:04 -0800

    fix: fixed MULTI_MAX: int = 100 in Settings in backend

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -11,6 +11,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_ignore_empty=True, extra="ignore"
     )
+    # Maximum number of items to return in paginated queries
+    MULTI_MAX: int = 100
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
     TOTP_SECRET_KEY: str = secrets.token_urlsafe(32)
